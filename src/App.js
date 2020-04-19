191a301585b0a7d1a523442021fe3b129b26da3a
d3jw <dejw.sedlar@gmail.com>
2020-04-19 22:21:59 +0200

    city displayed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Template from "./components/Template";
 import PlaceForm from "./components/PlaceForm";
 import ForecastCard from "./components/ForecastCard";
 import Loading from "./components/atoms/loading/Loading";
+import { H1 } from "./components/atoms/Headlines";
 
 import styled from "@emotion/styled";
 
@@ -22,6 +23,7 @@ const Wrapper = styled.div`
 const App = () => {
   const [forecast, setForecast] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [place, setPlace] = useState("");
 
   const onGetForecastClick = async (place) => {
     setForecast([]);
@@ -40,15 +42,21 @@ const App = () => {
 
       setLoading(false);
 
+      setPlace(place);
       setForecast(data);
     } catch (error) {
       //hack: GeoCode sometimes gives 403 even thought there is no auth, tell the user something nice
       if (error.message.includes("403")) {
+        setPlace("");
         setLoading(false);
         alert("Občas mi server pošle nepěknou odpověď, zkus to prosím znovu.");
       }
     }
   };
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
   const renderTiles = (data) =>
     data && data.map((frcs) => <ForecastCard key={frcs.time} item={frcs} />);
@@ -57,6 +65,11 @@ const App = () => {
     <Template>
       <PlaceForm onGetForecastClick={onGetForecastClick} />
       {loading ? <Loading /> : <div></div>}
+      {!loading && forecast !== null ? (
+        <H1 style={{ marginLeft: "5vw" }}>{capitalizeFirstLetter(place)}</H1>
+      ) : (
+        <div />
+      )}
       <Wrapper> {renderTiles(forecast)}</Wrapper>
     </Template>
   );
