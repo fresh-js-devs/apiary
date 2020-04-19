d4e72ac9a86508ecd513da55ce8bdb2d15bee2e4
d3jw <dejw.sedlar@gmail.com>
2020-04-19 18:47:29 +0200

    correct response get

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+
+import axios from "axios";
 
 import Template from "./components/Template";
 import PlaceForm from "./components/PlaceForm";
+import ForecastCard from "./components/ForecastCard";
+const key = "5a32783cab327da3665110a93ecf2117";
 
 const App = () => {
-  const onGetForecastClick = (option) => console.log(option);
+  const [forecast, setForecast] = useState([]);
+
+  const onGetForecastClick = async (place) => {
+    const { latt, longt } = await axios
+      .get(`https://geocode.xyz/${place}?json=1`, "jsonp")
+      .then((response) => response.data);
+
+    //cors policy at DarkSky
+    const { data } = await axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${key}/${latt},${longt}?units=si`
+      )
+      .then((response) => response.data.hourly);
+
+    setForecast(data);
+    console.log(forecast);
+  };
+
+  const renderTiles = data =>
+  data &&
+  data.map(frcs => (
+    <ForecastCard
+      key={frcs.time}
+      item={frcs}
+    />
+  ));
 
   return (
     <Template>
       <PlaceForm onGetForecastClick={onGetForecastClick} />
+      {renderTiles(forecast)}
     </Template>
   );
 };
