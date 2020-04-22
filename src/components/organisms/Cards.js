3ed95abf8837824630e05bf7b49fcd4fc1cf6e61
batlicka <vojta@vagunda.eu>
2020-04-22 08:39:39 +0200

    pictures

--- src/components/organisms/Cards.js
+++ src/components/organisms/Cards.js
@@ -1,34 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { useHistory } from 'react-router-dom';
 
 import CardImg from '../atoms/CardImg';
+import Container from '../atoms/Container';
 
 const CardsWrapper = styled.div`
   margin-top: 2rem;
 `;
 
-const Cards = ({data}) => {
-  const { push } = useHistory();
+function timeConverter(UNIX_timestamp) {
+    let a = new Date(UNIX_timestamp * 1000);
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let year = a.getFullYear();
+    let month = months[a.getMonth()];
+    let date = a.getDate();
+    let hour = a.getHours();
+    let min = a.getMinutes();
+    let sec = a.getSeconds();
+    let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+    return time;
+}
+const Cards = ({ data }) => {
+    return (
+        //pozn. 1) id převzate z card.id 2) alt = title obrazku
+        <CardsWrapper>
+            {/* <Container>
+                {data.td}
+                {data.weather[0].description}
+            </Container> */}
+            <Container>
+                <img src="/03n.png" alt="image" />
+                <span>there should be show weater: <strong>test</strong></span>
+                
+            </Container>
 
-  const handleGoToPokemonDetail = pokemonId => push(`/pokemon/${pokemonId}`);
+        </CardsWrapper>
 
-  return (
-    //pozn. 1) id převzate z card.id 2) alt = title obrazku
-    <CardsWrapper>
-      {data.map(card=>(      
-        <CardImg   
-          key={card.id}
-          alt={card.id}
-          src={card.imageUrl}       
-          className='card'
-          width='240'
-          height='330'
-          onClick={() => handleGoToPokemonDetail(card.id)}
-        />
-      ))};
-    </CardsWrapper>
-  );
+    );
 };
 
 export default Cards;
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -5,70 +5,81 @@ import Layout from '../components/organisms/Layout';
 import SearchBar from '../components/molecules/SearchBar';
 import Cards from '../components/organisms/Cards';
 
+
 const MainPage = () => {
-  const [weatherData, setWeatherData] = useState({
-    cards: [], 
-    isLoading: false,
-    error: '',
-  });
+    const [weatherData, setWeatherData] = useState({
+        cards: [],
+        isLoading: false,
+        error: '',
+    });
 
-  
-function timeConverter(UNIX_timestamp){
-    let a = new Date(UNIX_timestamp * 1000);
-    let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-    let year = a.getFullYear();
-    let month = months[a.getMonth()];
-    let date = a.getDate();
-    let hour = a.getHours();
-    let min = a.getMinutes();
-    let sec = a.getSeconds();
-    let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-    return time;
-}
+    const [cityName, setCityName] = useState('');              //hodnota na inputu, bude se předávat axiosu
 
+    const handleChange = event => setCityName(event.target.value);
 
-  const [cityName, setCityName] = useState('');              //hodnota na inputu, bude se předávat axiosu
+    //async = asynchronní volání
+    const handleSearchWeather = async () => {
+        try {
+            setWeatherData({
+                ...weatherData,
+                isLoading: true,
+            });
+            //default: b7ab5ba9b741e852fdfa609360891e1b
+            //personal key:af2629b621d06922de199e6d764c182f
+            const result = await axios(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=b7ab5ba9b741e852fdfa609360891e1b`);
+            console.log("content of response:")
+            console.log(result)
+            console.log("content of array: ")
+            console.log(result.data.list);
+            console.log("access to first item: ")
+            console.log(result.data.list[1].td);
+            console.log(result.data.list[1].weather[0].description);
 
-  const handleChange = event => setCityName(event.target.value);
+            const { cards } = result.data.list;
+            
+            setWeatherData({
+                ...weatherData,
+                cards,
+                isLoading: false,
+            })
+        } catch (error) {
 
-  //async = asynchronní volání
-const handleSearchWeather = async () => {
-    try {
-        setWeatherData({
-            ...weatherData,
-            isLoading: true,
-        });
-        //default: b7ab5ba9b741e852fdfa609360891e1b
-        //personal key:af2629b621d06922de199e6d764c182f
-        const result = await axios(`api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=b7ab5ba9b741e852fdfa609360891e1b`);
-        
-        console.log(result);
-        const{cards} = result;   
-    
-        setWeatherData({
-            ...weatherData,
-            cards,
-            isLoading: false,
-        })
-    } catch (error) {
-        
-    }
-  };
-  const renderCards = () => {
-   
-  };
+        }
+    };
+    console.log("saved in weatherData: ")
+    console.log({weatherData.cards})
+    const renderCards = () => {
+        if (weatherData.isLoading) {
+            return <div>Loading...</div>;
+        }
 
-  return (
-    <Layout>
-        <h1>metaweather.com API</h1>
-      <SearchBar
-        handleChange={handleChange}
-        cityName ={cityName}
-        handleSearchWeather={handleSearchWeather}
-      />
-      {renderCards()}
-    </Layout>
-  );
+        if (weatherData.error) {
+            return <div>{weatherData.error}</div>;
+        }
+
+        if (!weatherData.cards) {
+            return <div>No weather found!</div>;
+        }
+        // return (
+        //     {weatherData.cards.map(data=>(
+        //         <Cards data={data} />
+        //     ))}
+        // )
+        return <Cards data={weatherData.cards} />;
+    };
+
+    return (
+        <Layout>
+            <h1>metaweather.com API</h1>
+            <SearchBar
+                handleChange={handleChange}
+                cityName={cityName}
+                handleSearchWeather={handleSearchWeather}
+            />
+            
+            {renderCards()}
+        </Layout>
+    );
 };
 
 export default MainPage;
