31ee26e2a87bfcb8f2406c9c4aac7a7c64b1eecb
Juraj Frolo <jurajfrolo1@gmail.com>
2020-04-23 02:12:16 +0200

    1.0

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import Layout from '../components/organisms/Layout';
@@ -7,20 +7,21 @@ import LayoutButton from '../components/atoms/LayoutButton';
 const MainPage = () => {
     const[productData, setProductData] = useState({product: []});
     
-    const searchData = async () => {
-          await axios(
-            `http://localhost:5000/api/home`,
-          ).then(result => {
-              setProductData({
-                    product: result.data
-              });
-          });
-      };
+    useEffect(() => {
+        const searchData = async () => {
+            await axios(
+              `http://localhost:5000/api/home`,
+            ).then(result => {
+                setProductData({
+                      product: result.data
+                });
+            });
+        };
+        searchData();
+    }, []);
 
   return(
-      <Layout data={productData.product}>
-          <LayoutButton onClick={searchData}>Load Data</LayoutButton>
-      </Layout>
+      <Layout data={productData.product}/>
   );
 };
 
